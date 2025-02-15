b905232c733eb188b1e49a17bf1ab0e213fad312
Emir Dizdarevic <emir.dizdarevic@km.kongsberg.com>
2025-02-15 21:57:33 +0100

    Added vector tile posibility library

---
+++ VectorTileRender/Helpers/ColorHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Media;
+
+namespace VectorTileRenderer.Helpers;
+
+public static class ColorHelper
+{
+   public static Color HslToRgb(double h, double s, double l)
+    {
+        return FromHsl(h, s, l);
+    }
+
+    public static Color FromHsl(double h, double s, double l)
+    {
+        double r, g, b;
+
+        if (s == 0)
+        {
+            r = g = b = l;
+        }
+        else
+        {
+            double q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            double p = 2 * l - q;
+            r = HueToRgb(p, q, h + 1.0 / 3.0);
+            g = HueToRgb(p, q, h);
+            b = HueToRgb(p, q, h - 1.0 / 3.0);
+        }
+
+        return Color.FromArgb(255, (byte)(r * 255), (byte)(g * 255), (byte)(b * 255));
+    }
+
+    public static double HueToRgb(double p, double q, double t)
+    {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1.0 / 6.0) return p + (q - p) * 6 * t;
+        if (t < 1.0 / 2.0) return q;
+        if (t < 2.0 / 3.0) return p + (q - p) * (2.0 / 3.0 - t) * 6;
+        return p;
+    }
+}
---
+++ VectorTileRender/Sources/MbTilesSource.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VectorTileRenderer.Sources
+{
+    // MbTiles loading code in GIST by geobabbler
+    // https://gist.github.com/geobabbler/9213392
+
+    public class MbTilesSource : IVectorTileSource
+    {
+        public GlobalMercator.GeoExtent Bounds { get; private set; }
+        public GlobalMercator.CoordinatePair Center { get; private set; }
+        public int MinZoom { get; private set; }
+        public int MaxZoom { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string MBTilesVersion { get; private set; }
+        public string Path { get; private set; }
+
+        ConcurrentDictionary<string, VectorTile> tileCache = new ConcurrentDictionary<string, VectorTile>();
+
+        private GlobalMercator gmt = new GlobalMercator();
+
+        SQLiteConnection sharedConnection;
+
+
+        public MbTilesSource(string path)
+        {
+            this.Path = path;
+
+            sharedConnection = new SQLiteConnection(String.Format("Data Source={0};Version=3;Mode=ReadOnly", this.Path));
+            sharedConnection.Open();
+
+            loadMetadata();
+        }
+
+        private void loadMetadata()
+        {
+            try
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand() { Connection = sharedConnection, CommandText = "SELECT * FROM metadata;" })
+                {
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string name = reader["name"].ToString();
+                        switch (name.ToLower())
+                        {
+                            case "bounds":
+                                string val = reader["value"].ToString();
+                                string[] vals = val.Split(new char[] { ',' });
+                                double.TryParse(vals[0],NumberStyles.Any, CultureInfo.InvariantCulture, out double west);
+                                double.TryParse(vals[1], NumberStyles.Any, CultureInfo.InvariantCulture, out double south);
+                                double.TryParse(vals[2], NumberStyles.Any, CultureInfo.InvariantCulture, out double east);
+                                double.TryParse(vals[3], NumberStyles.Any, CultureInfo.InvariantCulture, out double north);
+                                this.Bounds = new GlobalMercator.GeoExtent() { West = west, South = south, East = east, North = north };
+                                break;
+                            case "center":
+                                val = reader["value"].ToString();
+                                vals = val.Split(new char[] { ',' });
+                                double.TryParse(vals[0], NumberStyles.Any, CultureInfo.InvariantCulture, out double x);
+                                double.TryParse(vals[1], NumberStyles.Any, CultureInfo.InvariantCulture, out double y);
+                                this.Center = new GlobalMercator.CoordinatePair() { X = x, Y = y };
+                                break;
+                            case "minzoom":
+                                this.MinZoom = Convert.ToInt32(reader["value"]);
+                                break;
+                            case "maxzoom":
+                                this.MaxZoom = Convert.ToInt32(reader["value"]);
+                                break;
+                            case "name":
+                                this.Name = reader["value"].ToString();
+                                break;
+                            case "description":
+                                this.Description = reader["value"].ToString();
+                                break;
+                            case "version":
+                                this.MBTilesVersion = reader["value"].ToString();
+                                break;
+
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new MemberAccessException("Could not load Mbtiles source file");
+            }
+        }
+
+        public Stream GetRawTile(int x, int y, int zoom)
+        {
+            try
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand() { Connection = sharedConnection, CommandText = String.Format("SELECT * FROM tiles WHERE tile_column = {0} and tile_row = {1} and zoom_level = {2}", x, y, zoom) })
+                {
+                    SQLiteDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        var stream = reader.GetStream(reader.GetOrdinal("tile_data"));
+                        return stream;
+                    }
+                }
+            }
+            catch
+            {
+                throw new MemberAccessException("Could not load tile from Mbtiles");
+            }
+
+            return null;
+        }
+
+        public void ExtractTile(int x, int y, int zoom, string path)
+        {
+            if (File.Exists(path))
+                System.IO.File.Delete(path);
+
+            using (var fileStream = File.Create(path))
+            using (Stream tileStream = GetRawTile(x, y, zoom))
+            {
+                tileStream.Seek(0, SeekOrigin.Begin);
+                tileStream.CopyTo(fileStream);
+            }
+        }
+
+        public async Task<VectorTile> GetVectorTile(int x, int y, int zoom)
+        {
+            var extent = new Rect(0, 0, 1, 1);
+            bool overZoomed = false;
+
+            if(zoom > MaxZoom)
+            {
+                var bounds = gmt.TileLatLonBounds(x, y, zoom);
+
+                var northEast = new GlobalMercator.CoordinatePair();
+                northEast.X = bounds.East;
+                northEast.Y = bounds.North;
+
+                var northWest = new GlobalMercator.CoordinatePair();
+                northWest.X = bounds.West;
+                northWest.Y = bounds.North;
+
+                var southEast = new GlobalMercator.CoordinatePair();
+                southEast.X = bounds.East;
+                southEast.Y = bounds.South;
+
+                var southWest = new GlobalMercator.CoordinatePair();
+                southWest.X = bounds.West;
+                southWest.Y = bounds.South;
+
+                var center = new GlobalMercator.CoordinatePair();
+                center.X = (northEast.X + southWest.X) / 2;
+                center.Y = (northEast.Y + southWest.Y) / 2;
+
+                var biggerTile = gmt.LatLonToTile(center.Y, center.X, MaxZoom);
+
+                var biggerBounds = gmt.TileLatLonBounds(biggerTile.X, biggerTile.Y, MaxZoom);
+
+                var newL = Utils.ConvertRange(northWest.X, biggerBounds.West, biggerBounds.East, 0, 1);
+                var newT = Utils.ConvertRange(northWest.Y, biggerBounds.North, biggerBounds.South, 0, 1);
+
+                var newR = Utils.ConvertRange(southEast.X, biggerBounds.West, biggerBounds.East, 0, 1);
+                var newB = Utils.ConvertRange(southEast.Y, biggerBounds.North, biggerBounds.South, 0, 1);
+
+                extent = new Rect(new Point(newL, newT), new Point(newR, newB));
+                //thisZoom = MaxZoom;
+
+                x = biggerTile.X;
+                y = biggerTile.Y;
+                zoom = MaxZoom;
+
+                overZoomed = true;
+            }
+            
+            try
+            {
+                var actualTile = await getCachedVectorTile(x, y, zoom);
+
+                if (actualTile != null)
+                {
+                    actualTile.IsOverZoomed = overZoomed;
+                    actualTile = actualTile.ApplyExtent(extent);
+                }
+
+                return actualTile;
+
+            } catch(Exception e)
+            {
+                return null;
+            }
+        }
+
+        async Task<VectorTile> getCachedVectorTile(int x, int y, int zoom)
+        {
+            var key = x.ToString() + "," + y.ToString() + "," + zoom.ToString();
+
+            lock(key)
+            {
+                if (tileCache.ContainsKey(key))
+                {
+                    return tileCache[key];
+                }
+
+                using (var rawTileStream = GetRawTile(x, y, zoom))
+                {
+                    var pbfTileProvider = new PbfTileSource(rawTileStream);
+                    var tile = pbfTileProvider.GetVectorTile(x, y, zoom).Result;
+                    tileCache[key] = tile;
+
+                    return tile;
+                }
+            }
+            
+        }
+
+        async Task<Stream> ITileSource.GetTile(int x, int y, int zoom)
+        {
+            return GetRawTile(x, y, zoom);
+        }
+    }
+}
