ae639891801461a2cf8004bf2894472dc199e669
Emir Dizdarevic <emir.dizdarevic@km.kongsberg.com>
2025-04-22 14:21:59 +0200

    Added vector tile source and provider Removed .net48 support

---
+++ MBTiles/WPF/MBVectorTileLayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace MapControl.MBTiles
+{
+    public class MBVectorTileLayer : MapTileLayer
+    {
+        public static readonly DependencyProperty FileProperty =
+            DependencyPropertyHelper.Register<MBVectorTileLayer, string>(nameof(File), null,
+                async (layer, oldValue, newValue) => await layer.FilePropertyChanged(newValue));
+
+        public string File
+        {
+            get => (string)GetValue(FileProperty);
+            set => SetValue(FileProperty, value);
+        }
+
+        public static readonly DependencyProperty StyleProperty =
+            DependencyPropertyHelper.Register<MBVectorTileLayer, string>(nameof(StylePath), null,
+                async (layer, oldValue, newValue) => await layer.StylePathPropertyChanged(newValue));
+
+        public string StylePath
+        {
+            get;
+            set;
+        }
+
+        protected virtual async Task<MBVectorTileSource> CreateTileSourceAsync(string file)
+        {
+            var tileSource = new MBVectorTileSource();
+
+
+            return tileSource;
+        }
+
+        private async Task StylePathPropertyChanged(string file)
+        {
+            (TileSource as MBVectorTileSource)?.Close();
+
+            ClearValue(TileSourceProperty);
+            ClearValue(SourceNameProperty);
+            ClearValue(DescriptionProperty);
+            ClearValue(MinZoomLevelProperty);
+            ClearValue(MaxZoomLevelProperty);
+
+
+
+        }
+
+        private async Task FilePropertyChanged(string file)
+        {
+            (TileSource as MBVectorTileSource)?.Close();
+
+            ClearValue(TileSourceProperty);
+            ClearValue(SourceNameProperty);
+            ClearValue(DescriptionProperty);
+            ClearValue(MinZoomLevelProperty);
+            ClearValue(MaxZoomLevelProperty);
+        }
+
+    }
+}
--- MBTiles/WPF/MBVectorTileSource.cs
+++ MBTiles/WPF/MBVectorTileSource.cs
@@ -1,7 +1,43 @@
-﻿namespace MapControl.MBTiles
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MapControl.MBTiles
 {
-    public class MBVectorTileSource
+    public class MBVectorTileSource : TileSource, IDisposable
     {
-        
+
+        //public MBVectorTileSource(string path, string stylePath, string cachePath)
+        //{
+            
+        //}
+
+
+
+        public void Close()
+        {
+
+        }
+
+        public override async Task<ImageSource> LoadImageAsync(int x, int y, int zoomLevel)
+        {
+            ImageSource image = null;
+
+            return image;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //Close();
+            }
+        }
     }
 }
